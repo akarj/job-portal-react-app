66b9eb8ac76f2e2e3b1bc1797d5231b1cb0f58b6
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-11-14 15:05:53 +0530

    React-router imstalled!!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Job Portal App</h1>
+      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}>
+            {/* <Route path="expenses" element={<Expenses />} />
+            <Route path="invoices" element={<Invoices />}>
+              
+              <Route path=":invoiceId" element={<Invoice />} />
+            </Route> */}
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
